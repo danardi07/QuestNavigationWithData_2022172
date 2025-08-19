ebdae865489578fe63c501cf22c8f9d1ac1f86b6
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2025-08-19 16:27:34 +0700

    membuat DataJK untuk model penyimpanan mvvm

---
+++ app/src/main/java/com/example/architecturemvvm/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.architecturemvvm.model
+
+import com.example.architecturemvvm.R
+
+object DataJK {
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.perempuan
+    )
+}
